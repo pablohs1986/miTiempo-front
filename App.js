ef05b443e03690909953b88fc9347d3a33688974
pablohs1986 <pabloherrero1986@gmail.com>
2021-04-11 19:59:52 +0200

    Implemented navigation stack+sign up screen design

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import AccountScreen from './src/screens/AccountScreen';
+import SigninScreen from './src/screens/SigninScreen';
+import SignupScreen from './src/screens/SignupScreen';
+import TaskCreateScreen from './src/screens/TaskCreateScreen';
+import TaskDetailScreen from './src/screens/TaskDetailScreen';
+import TaskListScreen from './src/screens/TaskListScreen';
+import TaskTimerScreen from './src/screens/TaskTimerScreen';
+import TrackerScreen from './src/screens/TrackerScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+/** Navegation */
+const switchNavigator = createSwitchNavigator({
+	loginFlow: createStackNavigator({
+		Signup: SignupScreen,
+		Signin: SigninScreen,
+	}),
+	mainFlow: createBottomTabNavigator({
+		homeFlow: createStackNavigator({
+			TaskList: TaskListScreen,
+			TaskDetail: TaskDetailScreen,
+			TaskTimer: TaskTimerScreen,
+		}),
+		TaskTime: TrackerScreen,
+		TaskCreate: TaskCreateScreen,
+		Account: AccountScreen,
+	}),
 });
+
+const App = createAppContainer(switchNavigator);
+
+export default () => {
+	return <App />;
+};
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,14 +1,95 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, Input, Button, Image } from 'react-native-elements';
+import { SafeAreaView } from 'react-navigation';
+import Spacer from '../components/Spacer';
+import { FontAwesome } from '@expo/vector-icons';
 
 const SignupScreen = () => {
 	return (
-		<View>
-			<Text>SignupScreen</Text>
-		</View>
+		<SafeAreaView style={styles.container} forceInset={{ top: 'always' }}>
+			<Spacer>
+				<Image
+					source={require('../../assets/img/logoMiTiempo.png')}
+					containerStyle={styles.logo}
+				/>
+			</Spacer>
+
+			<Spacer>
+				<Input
+					autoCapitalize="none"
+					autoCorrect={false}
+					placeholder="Email"
+				/>
+				<Input
+					autoCapitalize="none"
+					autoCorrect={false}
+					placeholder="Password"
+				/>
+				<Button buttonStyle={styles.solidButton} title="Sign up" />
+				<Button
+					buttonStyle={styles.outlineButton}
+					titleStyle={styles.titleColorOutlineButton}
+					type="outline"
+					title="Sign up with "
+					icon={
+						<FontAwesome name="google" style={styles.googleIcon} />
+					}
+					iconRight
+				/>
+			</Spacer>
+			<Spacer>
+				<TouchableOpacity
+					onPress={() => navigation.navigate(routeName)}
+				>
+					<Text style={styles.link}>
+						Already have an account? Sign in instead!
+					</Text>
+				</TouchableOpacity>
+			</Spacer>
+		</SafeAreaView>
 	);
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+	return {
+		headerShown: false,
+		cardStyle: { backgroundColor: 'white' },
+	};
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1, // rellena todo el alto
+		justifyContent: 'flex-start',
+	},
+	logo: {
+		width: 200,
+		height: 200,
+		alignSelf: 'center',
+		marginTop: 10,
+		marginBottom: 5,
+	},
+	solidButton: {
+		backgroundColor: '#C830CC',
+		marginTop: 5,
+		marginBottom: 10,
+	},
+	outlineButton: {
+		borderColor: '#C830CC',
+		borderWidth: 1,
+	},
+	titleColorOutlineButton: {
+		color: '#C830CC',
+	},
+	link: {
+		color: '#C830CC',
+	},
+	googleIcon: {
+		marginLeft: 5,
+		fontSize: 24,
+		color: '#C830CC',
+	},
+});
 
 export default SignupScreen;

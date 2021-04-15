4bed9e7117d4298d5c92e4c9593c2c62f94bc8f8
pablohs1986 <pabloherrero1986@gmail.com>
2021-04-15 10:07:37 +0200

    AccountScreenForm deleted, a separate component is not necessary.

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -2,8 +2,8 @@ import React, { useContext, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text, Input, Button, Divider } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
+import Spacer from '../components/Spacer';
 import { FontAwesome5 } from '@expo/vector-icons';
-import AccountForm from '../components/AccountForm';
 import { Context as AuthContext } from '../context/AuthContext';
 
 const AccountScreen = () => {
@@ -11,7 +11,70 @@ const AccountScreen = () => {
 
 	return (
 		<SafeAreaView style={styles.container} forceInset={{ top: 'always' }}>
-			<AccountForm signout={signout}></AccountForm>
+			<Text style={styles.header} h4>
+				My Account
+			</Text>
+
+			<Spacer>
+				<Input
+					autoCapitalize="none"
+					autoCorrect={false}
+					placeholder="Email"
+					// value="{email}"
+					onChangeText="{setEmail}"
+				/>
+
+				<Input
+					autoCapitalize="none"
+					autoCorrect={false}
+					placeholder="Name"
+					// value="{email}"
+					onChangeText="{setEmail}"
+				/>
+				<Input
+					autoCapitalize="none"
+					autoCorrect={false}
+					placeholder="City"
+					// value="{password}"
+					onChangeText="{setPassword}"
+				/>
+
+				<Divider style={styles.divider} />
+
+				<Input
+					autoCapitalize="none"
+					autoCorrect={false}
+					placeholder="New password"
+					// value="{password}"
+					onChangeText="{setPassword}"
+				/>
+				<Input
+					autoCapitalize="none"
+					autoCorrect={false}
+					placeholder="Confirm password"
+					// value="{password}"
+					onChangeText="{setPassword}"
+				/>
+				<Spacer />
+				{/* {errorMessage ? (
+					<Text style={styles.errorMessage}>{errorMessage}</Text>
+				) : null} */}
+				<Button
+					buttonStyle={styles.solidButton}
+					title="Update"
+					// onPress={() => onSubmit({ email, password })}
+					// onPress={toggleOverlay}
+				/>
+				<Button
+					buttonStyle={styles.outlineButton}
+					titleStyle={styles.titleColorOutlineButton}
+					type="outline"
+					title="Sign out"
+					// icon={icon}
+					iconRight
+					onPress={signout}
+				/>
+			</Spacer>
 		</SafeAreaView>
 	);
 };
@@ -26,6 +89,32 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		justifyContent: 'flex-start',
+	},
+	header: {
+		marginTop: 10,
+		alignSelf: 'center',
+	},
+	divider: {
+		width: '100%',
+		alignSelf: 'center',
+		margin: 10,
+	},
+	solidButton: {
+		backgroundColor: '#C830CC',
+		marginBottom: 10,
+	},
+	outlineButton: {
+		borderColor: 'red',
+		borderWidth: 1,
+	},
+	titleColorOutlineButton: {
+		color: 'red',
+	},
+	errorMessage: {
+		alignSelf: 'center',
+		fontSize: 16,
+		color: 'red',
+		marginBottom: 15,
 	},
 });
 

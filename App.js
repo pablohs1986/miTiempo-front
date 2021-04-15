12bc2931f5f00517e1423cc880d7f8a909904b8e
pablohs1986 <pabloherrero1986@gmail.com>
2021-04-15 19:07:10 +0200

    Added LogBox.ignore to ignore logs on dev device.

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { LogBox } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import appNavigator from './src/navigation/appNavigator';
 import { setNavigator } from './src/navigation/externalNavigator';
 import { Provider as AuthProvider } from './src/context/AuthContext';
 import { Provider as UserProvider } from './src/context/UserContext';
+
+LogBox.ignoreAllLogs(); // Ignore all log notifications on dev device
 
 const App = createAppContainer(appNavigator());
 
